0a9c6474e0d7063ace4848ce550828291c7bd067
josemariabyu <jmsanabria@outlook.com>
2024-07-01 14:28:01 -0300

    Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,6 +1,6 @@
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args)//Scripture Memorizer
     {
         Reference reference = new Reference("John", 3, 16);
         string text = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
