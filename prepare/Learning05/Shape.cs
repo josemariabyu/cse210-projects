8aae65c84cc96b7f08ee1ad07f51d9f0800e9b45
josemariabyu <jmsanabria@outlook.com>
2024-07-12 16:10:19 -0300

    Shapes

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,19 @@
+using System;
+
+abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    // Abstract method to get the area of the shape
+    public abstract double GetArea();
+}
